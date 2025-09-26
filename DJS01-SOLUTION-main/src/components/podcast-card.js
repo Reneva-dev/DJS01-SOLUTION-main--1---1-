4046899abedd45f3097dc1b5941b865a731c23bd
Reneva Newman <renevanewman7@gmail.com>
2025-09-26 20:15:31 +0300

    added extension and return value for HTMLElement

--- DJS01-SOLUTION-main/src/components/podcast-card.js
+++ DJS01-SOLUTION-main/src/components/podcast-card.js
@@ -1,2 +1,7 @@
 import { GenreService } from "../utils/GenreService.js";
 import { DateUtils } from "../utils/DateUtils.js";
+
+class PodcastCard extends HTMLElement {
+  static get observedAttributes() {
+    return ["title", "image", "seasons", "genres", "updated"];
+  }
